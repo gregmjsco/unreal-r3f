c7e9532e544d7030c0995370a2682defd8c23f1f
Greg Johnston <gregmj.sco@gmail.com>
2025-01-29 12:57:48 +0900

    Implement orbit controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,34 @@
+import { useThree, extend, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+extend({ OrbitControls });
+
 export default function Experience() {
+  const cubeRef = useRef();
+  const groupRef = useRef();
+
+  const { camera, gl } = useThree();
+
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta;
+    groupRef.current.rotation.y += delta;
+  });
+
   return (
     <>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshBasicMaterial color="orange" />
-      </mesh>
+      <orbitControls args={[camera, gl.domElement]} />
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color="orange" />
+        </mesh>
 
-      <mesh position={[2, 0, 0]} scale={1.5}>
-        <boxGeometry />
-        <meshBasicMaterial color="mediumpurple" wireframe />
-      </mesh>
+        <mesh ref={cubeRef} position={[2, 0, 0]} scale={1.5}>
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" wireframe />
+        </mesh>
+      </group>
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
