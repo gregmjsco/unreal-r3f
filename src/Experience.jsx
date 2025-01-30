31d989d1d38868e83deb6da5f18b5bd7ec753587
Greg Johnston <gregmj.sco@gmail.com>
2025-01-30 10:31:52 +0900

    Add pivot controls to sphere, transform controls to the cube and add some Html elements with occlude on

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,41 +1,56 @@
 import { useThree, extend, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import CustomObject from "./CustomObject";
-
-extend({ OrbitControls });
+import {
+  OrbitControls,
+  TransformControls,
+  PivotControls,
+  Html,
+} from "@react-three/drei";
 
 export default function Experience() {
   const cubeRef = useRef();
+  const sphereRef = useRef();
   const groupRef = useRef();
 
-  const { camera, gl } = useThree();
+  //   const { camera, gl } = useThree();
 
   useFrame((state, delta) => {
-    const angle = state.clock.elapsedTime;
-    state.camera.position.x = Math.sin(angle) * 8;
-    state.camera.position.z = Math.cos(angle) * 8;
-    state.camera.lookAt(0, 0, 0);
-
-    cubeRef.current.rotation.y += delta;
-    groupRef.current.rotation.y += delta;
+    // const angle = state.clock.elapsedTime;
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
+    // state.camera.lookAt(0, 0, 0);
+    // cubeRef.current.rotation.y += delta;
+    // groupRef.current.rotation.y += delta;
   });
 
   return (
     <>
-      {/* <orbitControls args={[camera, gl.domElement]} /> */}
+      <OrbitControls makeDefault />
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
       <group ref={groupRef}>
-        <mesh position-x={-2}>
-          <sphereGeometry />
-          <meshStandardMaterial color="orange" />
-        </mesh>
+        <PivotControls anchor={[0, 0, 0]} depthTest={false}>
+          <mesh ref={sphereRef} position-x={-2}>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+            <Html
+              position={[1, 1, 0]}
+              wrapperClass="label"
+              center
+              distanceFactor={4}
+              occlude={[sphereRef, cubeRef]}
+            >
+              That's a sphere 👍
+            </Html>
+          </mesh>
+        </PivotControls>
 
         <mesh ref={cubeRef} position={[2, 0, 0]} scale={1.5}>
           <boxGeometry />
           <meshStandardMaterial color="mediumpurple" />
         </mesh>
+        <TransformControls object={cubeRef} mode="rotate" />
       </group>
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
@@ -44,6 +59,8 @@ export default function Experience() {
       </mesh>
 
       <CustomObject />
+
+      <Html>Test</Html>
     </>
   );
 }
